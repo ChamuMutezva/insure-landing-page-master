b6c69faec438662e329cdb4f29b7288c5034ebf2
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-02-22 09:57:23 +0200

    Fix: items 4, 5, 6, 7,8. hover states, footer background image

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 const mobileNav = document.getElementById('main-nav');
 const hamburgerIcon = document.getElementsByClassName('hamburger-icon')[0];
 const closeIcon = document.getElementsByClassName('close-icon')[0];
+const body = document.body;
 
 hamburgerIcon.addEventListener('click', () => {
   mobileNav.classList.remove('hide');
   hamburgerIcon.classList.add('hide');
   closeIcon.classList.remove('hide');
+  body.style.overflowY = "hidden";
 });
 
 closeIcon.addEventListener('click', () => {
   mobileNav.classList.add('hide');
   hamburgerIcon.classList.remove('hide');
   closeIcon.classList.add('hide');
+  body.style.overflowY = "visible";
 });
 console.log(hamburgerIcon);
